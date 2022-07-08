2fd19e2bdf8c9978d3d1f89b867365a5f58cfe12
bijay-shrestha <8ijay.shrestha@gmail.com>
2022-07-07 21:48:22 -0500

    Initialized Spring Project

---
+++ src/main/java/edu/miu/eregistrarmvc/ERegistrarMvcApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.eregistrarmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegistrarMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERegistrarMvcApplication.class, args);
+    }
+
+}
